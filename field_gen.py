64506b649213562fcaf56acab357d7745de37d8b
Vlad Korzun <vlad.korzun.minsk@gmail.com>
2024-06-27 20:16:09 +0300

    add blocktypes and simple field gen

--- field_gen.py
+++ field_gen.py
@@ -2,14 +2,26 @@ from random import randint
 from pprint import pprint as print 
 from primitieves import *
 
-class Chunk():
-    field = [[0]*16]*16
+seed = 7687906
 
-    def __init__(self, p):
-        self.field = self.genField()
+class Chunk():
+    field = [[0]*16 for _ in range(16)]
+    p = Point()
+
+    def __init__(self, p, seed):
+        self.genField(p, seed)
+        self.p = p
     
-    def genField(self, p1, p2):
-        ...
+    def genField(self, p, seed):
+        width = (seed * (p.x + p.y) % 13 + 4) # Random staff
+        for i in range(8 - width // 2, 8 + width // 2):
+            for j in range(8 - width // 2, 8 + width // 2):
+                self.field[i][j] = 1 if i not in (8 - width // 2, 8 + width // 2 - 1) and j not in (8 - width // 2, 8 + width // 2 - 1) else 2
+
+    def __repr__(self):
+        print(f'Chunk {self.p}')
+        print(self.field)
+        return ''
 
 class Map:
     seed = -1
@@ -24,6 +36,7 @@ class Map:
         return self.field.__str__()
 
 
-p1 = Point(1,2)
+p1 = Point(1,0)
 p2 = Point(3,7)
-print(p1 * 2 + p2)
+c = Chunk(p1, seed)
+print(c)
